e662797499ae4b8bfe9dc16509b43be1c3691045
singleflare <dodinhdong28092002@gmail.com>
2023-11-25 10:37:41 +0700

    c

--- src/Components/TopNavBar.js
+++ src/Components/TopNavBar.js
@@ -1,17 +1,19 @@
 import React from "react"
 import './styles.css'
+import Button from './Button'
+import { Link } from 'react-router-dom';
 
 const TopNavBar=()=> {
   return(
   <div className="NavBar">
     <ul className='NavList'>
-      <li className="NavItem">Home</li>
-      <li className="NavItem">Employees</li>
-      <li className="NavItem">About</li>
+      <Link to='/'><li className="NavItem"><Button>Home</Button></li></Link>
+      <Link to='/employees'><li className="NavItem"><Button>Employees</Button></li></Link>
+      <Link to='/about'><li className="NavItem"><Button>About</Button></li></Link>
     </ul>
     <ul className='NavList'>
-      <li className="NavItem">Login</li>
-      <li className="NavItem">Register</li>
+      <li className="NavItem"><Button>Login</Button></li>
+      <li className="NavItem"><Button>Register</Button></li>
     </ul>
   </div>
   )
