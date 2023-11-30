9051c34a3e5b64d8d378218027e36d0ba2a7a87a
singleflare <dodinhdong28092002@gmail.com>
2023-11-30 14:17:37 +0700

    ok

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,16 +3,19 @@ import React from "react"
 const Login=()=>{
   return(
     <div className="Login">
-      <form action="" class="LoginForm">
+      <form action="" class="LoginForm" id="login">
         <div className="Username">
-          <label className="UsernameLabel" for="name">Username</label>
-          <input className="UsernameInput" type="text"></input>
+          <label className="UsernameLabel" for="username">Username</label>
+          <input className="UsernameInput" id="username" type="text"></input>
         </div>
         <div className="Password">
-          <label className="PasswordLabel" for="name">Password</label>
-          <input className="PasswordInput" type="text"></input>
+          <label className="PasswordLabel" for="password">Password</label>
+          <input className="PasswordInput" id="password" type="text"></input>
         </div>
+        <button type="submit" form="login" className="LoginBtn">Login</button>
       </form>
+      <a>Reset username/password</a>
+      <a>Not registered?</a>
     </div>
   )
 }
