39e6513117b84d4565a8584217ca0e9c8c1465aa
singleflare <dodinhdong28092002@gmail.com>
2024-01-13 09:47:19 +0700

    des

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,4 +1,25 @@
 import React from "react"
+import { Client } from 'pg'
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  database: 'employees',
+  user: 'admin',
+  password: 'dong2002'
+})
+await client.connect()
+ 
+try {
+  const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+  console.log(res.rows[0].message) // Hello world!
+} 
+catch (err) {
+  console.error(err);
+} 
+finally {
+  await client.end()
+}
 
 const EmployeeList=()=>{
   return(
