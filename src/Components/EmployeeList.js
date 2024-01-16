ed9fa94e68ca60a7bda6e8df0782f70fbe7c8d9f
singleflare <dodinhdong28092002@gmail.com>
2024-01-16 20:54:07 +0700

    props

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,15 +1,24 @@
 import React from "react"
 
+const employees = [
+  { name: 'Alice', age: 30 },
+  { name: 'Bob', age: 35 },
+  { name: 'Charlie', age: 40 }
+];
+
+const Employee=props=>{
+  return(
+    <div>{props.name}, {props.age}</div>
+  )
+}
 
 const EmployeeList=()=>{
   return(
-    <div>
-      <ul className="EmpList">
-        <li className="EmpItem">An item</li>
-        <li className="EmpItem">A second item</li>
-      </ul>
+    <div className="EmpEntry">
+      {employees.map((employee,i)=>(
+        <Employee name={employee.name} age={employee.age} />
+      ))}
     </div>
-    
   )
 }
 export default EmployeeList
--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ import Register from './Components/Register'
 
 export default[
     <Route key="welcome" path="/" element={<Welcome />}/>,
-    <Route key="employees" path="/employees" element={<EmployeeList />}/>,
+    <Route key="employees" path="/employees" element={<EmployeeList name="A" age="25" position="Director"/>}/>,
     <Route key="about" path="/about" element={<About />}/>,
     <Route key="login" path="/login" element={<Login />}/>,
     <Route key="register" path="/register" element={<Register />}/>
